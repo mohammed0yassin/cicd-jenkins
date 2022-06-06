b238c786f40d831ddd62164d5e80a7a2290e1ad3
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-06-06 15:14:11 +0200

    fix

--- groovy-pipeline/jenkins.groovy
+++ groovy-pipeline/jenkins.groovy
@@ -3,7 +3,7 @@ pipeline {
 
     tools {
         // Install the Maven version configured as "M3" and add it to the path.
-        maven "'apache-maven-3.8.1'"
+        maven "apache-maven-3.8.1"
     }
 
     stages {
