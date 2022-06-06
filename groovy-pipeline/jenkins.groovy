108b89ae3a667ce606c507c3eea6778dd4e2f7a3
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-06-06 15:01:25 +0200

    ADD: jenkins pipeline code

---
+++ groovy-pipeline/jenkins.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent none
+
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "M3"
+    }
+
+    stages {
+        stage('Run software on slave 1') {
+            agent { label 'slave1-build' }
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/mohammed0yassin/cicd-jenkins/tree/master/slave1-files'
+
+                // Run Maven on a Unix agent.
+                sh "mvn spring-boot:run"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+        }
+    },
+    stages {
+        stage('Run software on slave 2') {
+            agent { label 'slave2-build' }
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/mohammed0yassin/cicd-jenkins/tree/master/slave2-files'
+
+                // Run Maven on a Unix agent.
+                sh "mvn spring-boot:run"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+        }
+    }
+}
